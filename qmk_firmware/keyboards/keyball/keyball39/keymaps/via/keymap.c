a1e8fa62f6fdeed9254d23c9dcff643799a71292
yuukab <47452177+yuukab@users.noreply.github.com>
2024-09-02 15:28:09 +0000

    combo設定変更、レイヤー追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -50,6 +50,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
   ),
+
+  [4] = LAYOUT_universal(
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+  ),
 };
 // clang-format on
 
@@ -75,7 +82,7 @@ void oledkit_render_info_user(void) {
 
 
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM my_combo1[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM my_combo1[] = {LCTL_T(KC_F), LCTL_T(KC_J), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(my_combo1, KC_BTN1),
